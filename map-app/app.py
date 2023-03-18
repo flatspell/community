a9d5fc15a092f00f5e3f12865866921ebdcc8d1d
Lucas Wiley <22181786+lucaswiley@users.noreply.github.com>
2023-03-18 01:01:55 -0500

    cleanup unused code

--- map-app/app.py
+++ map-app/app.py
@@ -3,7 +3,6 @@ import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates"))
 
 @app.route('/')
 def home():
@@ -14,7 +13,6 @@ def census_data():
     # Fetch data from the Census Bureau API using the requests library
     # and return the data as a JSON object
     pass
-    e071b8af90ea338427066a202eeac7284dc62737
 
 if __name__ == '__main__':
     app.run(debug=True)
