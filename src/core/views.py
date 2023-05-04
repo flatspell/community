93fef9920e271d5115b7207414a0bf860b6f8d56
Lucas Wiley <22181786+lucaswiley@users.noreply.github.com>
2023-05-04 09:42:52 -0700

    update templates to use new navbar

--- src/core/views.py
+++ src/core/views.py
@@ -11,19 +11,19 @@ def home():
 @core_bp.route('/investment')
 @login_required
 def investment():
-    return render_template('core.investment.html')
+    return render_template('core/investment.html')
 
-# @core_bp.route('/commerce')
-# @login_required
-# def commerce():
-#     return render_template('commerce.html')
+@core_bp.route('/account')
+@login_required
+def account():
+    return render_template('core/account.html')
 
-# @core_bp.route('/community')
-# @login_required
-# def community():
-#     return render_template('community.html')
+@core_bp.route('/commerce')
+@login_required
+def commerce():
+    return render_template('commerce.html')
 
-# @core_bp.route('/local_wealth_fund')
-# @login_required
-# def local_wealth_fund():
-#     return render_template('local_wealth_fund.html')
+@core_bp.route('/community')
+@login_required
+def community():
+    return render_template('community.html')
