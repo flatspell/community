3562e7baaa34a3827e4480cf890902799108d89f
Lucas Wiley <22181786+lucaswiley@users.noreply.github.com>
2023-05-04 13:51:05 -0700

    fix error handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from decouple import config
-from flask import Flask
+from flask import Flask, render_template
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
