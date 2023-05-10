27dfd74f31a8ea8f33eddedf0d32f70398fabaf8
Lucas Wiley <22181786+lucaswiley@users.noreply.github.com>
2023-05-10 13:15:30 -0700

    update user model and run migrations

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -12,14 +12,18 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
-    created_on = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_at = db.Column(db.DateTime, nullable=True)
 
-    def __init__(self, email, password, is_admin=False):
+    def __init__(self, email, password, is_admin=False, is_confirmed=False, confirmed_at=None):
         self.email = email
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
-        self.created_on = datetime.now()
+        self.created_at = datetime.now()
         self.is_admin = is_admin
+        self.is_confirmed = is_confirmed
+        self.confirmed_at = confirmed_at
 
     def __repr__(self):
         return f"<email {self.email}>"
