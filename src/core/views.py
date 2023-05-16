008ad436e058ec678137c51332d214d4e4800a2c
Lucas Wiley <22181786+lucaswiley@users.noreply.github.com>
2023-05-16 09:24:50 -0700

    add roles accepted to each route

--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from flask_security import roles_accepted
 
 from src.utils.decorators import check_is_confirmed
 
@@ -8,29 +9,34 @@ core_bp = Blueprint("core", __name__)
 @core_bp.route("/")
 @login_required
 @check_is_confirmed
+@roles_accepted('investor', 'entrepreneur', 'economic_developer')
 def home():
     return render_template("core/index.html")
 
 @core_bp.route('/investment')
 @login_required
 @check_is_confirmed
+@roles_accepted('investor', 'entrepreneur', 'economic_developer')
 def investment():
     return render_template('core/investment.html')
 
 @core_bp.route('/account')
 @login_required
 @check_is_confirmed
+@roles_accepted('investor', 'entrepreneur', 'economic_developer')
 def account():
     return render_template('core/account.html')
 
 @core_bp.route('/commerce')
 @login_required
 @check_is_confirmed
+@roles_accepted('investor', 'entrepreneur', 'economic_developer')
 def commerce():
     return render_template('core/commerce.html')
 
 @core_bp.route('/community')
 @login_required
 @check_is_confirmed
+@roles_accepted('investor', 'entrepreneur', 'economic_developer')
 def community():
     return render_template('core/community.html')
